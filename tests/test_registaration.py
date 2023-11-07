94422dc6e5abd83028b99b3e1c8ce7be5122768e
Irina Vasilyeva <irishka.cdo@gmail.com>
2023-11-07 03:59:48 +0300

    fix github workflow

--- tests/test_registaration.py
+++ tests/test_registaration.py
@@ -16,32 +16,32 @@ class TestRegistrationPage:
         heading_text = heading.text
         assert heading_text == "Автотесты. Основная олимпиада", f"Заголовок регистрационной формы отсутствует " \
                                                                 f"или является некорректным: {heading_text}"
-    #
-    # @pytest.mark.smoke_test
-    # def test_01_02_registration_with_correct_data(self, driver, registration_page):
-    #     """Проверка возможности регистрации, когда все поля заполнены корректными данными"""
-    #
-    #     registration_page.get_surname_field().send_keys(credentials["surname"])
-    #     registration_page.get_name_field().send_keys(credentials["username"])
-    #     registration_page.get_patronymic_field().send_keys(credentials["patronymic"])
-    #     registration_page.get_date_field().send_keys(credentials["date_birth"])
-    #     time.sleep(3)
-    #     registration_page.get_email_field().send_keys(credentials["email"])
-    #     registration_page.get_vosh_login_field().send_keys(credentials["vosh_login"])
-    #     registration_page.get_phone_field().send_keys(credentials["phone"])
-    #     registration_page.get_snils_field().send_keys(credentials["snils"])
-    #     registration_page.get_profession_field().send_keys(credentials["profession"])
-    #     registration_page.get_country_field().click()
-    #     registration_page.get_choose_country().click()
-    #     registration_page.get_city_field().send_keys(credentials["city"])
-    #     registration_page.get_organisation_field().send_keys(credentials["organization"])
-    #     registration_page.get_school_field().send_keys(credentials["school"])
-    #     registration_page.get_class_field().send_keys(credentials["class"])
-    #     registration_page.get_checkbox_confirmation_of_veracity().click()
-    #     registration_page.get_checkbox_users_agreement_and_personal_data().click()
-    #     registration_page.get_checkbox_familiarized_with_the_rules().click()
-    #     assert registration_page.get_confirmation_button().is_enabled() is True, f"Кнопка отсутствует или неактивна"
-    #
+
+    @pytest.mark.smoke_test
+    def test_01_02_registration_with_correct_data(self, driver, registration_page):
+        """Проверка возможности регистрации, когда все поля заполнены корректными данными"""
+
+        registration_page.get_surname_field().send_keys(credentials["surname"])
+        registration_page.get_name_field().send_keys(credentials["username"])
+        registration_page.get_patronymic_field().send_keys(credentials["patronymic"])
+        registration_page.get_date_field().send_keys(credentials["date_birth"])
+        time.sleep(3)
+        registration_page.get_email_field().send_keys(credentials["email"])
+        registration_page.get_vosh_login_field().send_keys(credentials["vosh_login"])
+        registration_page.get_phone_field().send_keys(credentials["phone"])
+        registration_page.get_snils_field().send_keys(credentials["snils"])
+        registration_page.get_profession_field().send_keys(credentials["profession"])
+        registration_page.get_country_field().click()
+        registration_page.get_choose_country().click()
+        registration_page.get_city_field().send_keys(credentials["city"])
+        registration_page.get_organisation_field().send_keys(credentials["organization"])
+        registration_page.get_school_field().send_keys(credentials["school"])
+        registration_page.get_class_field().send_keys(credentials["class"])
+        registration_page.get_checkbox_confirmation_of_veracity().click()
+        registration_page.get_checkbox_users_agreement_and_personal_data().click()
+        registration_page.get_checkbox_familiarized_with_the_rules().click()
+        assert registration_page.get_confirmation_button().is_enabled() is True, f"Кнопка отсутствует или неактивна"
+
     # @pytest.mark.smoke_test
     # def test_01_03_registration_with_all_empty_fields(self, driver, registration_page):
     #     """Проверка возможности регистрации, когда все поля не заполнены"""
